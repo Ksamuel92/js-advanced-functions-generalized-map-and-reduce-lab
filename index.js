4c4b5f2e3ad6d0bdb9ce15cda5ff9b2bcfcb0dbe
Kyle Samuel <ksamuel92@gmail.com>
2021-09-09 12:21:02 -0400

    commit tree 0240e3056dfdea277a6a607fc5d0ba4e0d85cb6b

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Add your functions here
+
+function map (array, fn) {
+  const newArray = []
+  for(const element of array) {
+    newArray.push(fn(element));
+  }
+  return newArray;
+}
+
+function reduce (array, fn, startingValue){
+  let runningTotal = startingValue;
+  let memo = fn();
+  for(const element of array) {
+    runningTotal += fn(element, memo)
+  }
+  return runningTotal;
+}
